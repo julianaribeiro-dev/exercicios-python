0914d9df1ff2eca39b27517b4b1f1c475484bfb4
Juliana Ribeiro <jmbrito.ribeirodev@gmail.com>
2026-04-27 21:44:36 -0300

    feature: respondendo exercícios em python

---
+++ area-de-um-circulo.py
@@ -0,0 +1,6 @@
+#Faça um programa que peça o raio de um círculo, calcule e mostre sua área.
+
+pi = 3.14
+raio = float(input('Para descobrir a área de um circulo, determine o valor do raio: '))
+area = pi * raio**2
+print(area)
---
+++ conversor-metro-centimetros.py
@@ -0,0 +1,5 @@
+#Faça um programa que converta metros para centímetros.
+
+metro = float(input("Informe o valor do metro: "))
+centimetro = metro * 100
+print('O valor de', metro, 'metro(s) para centimetro(s), é igual a: ',centimetro,'centimetro(s).')
