555be7f685f30706f29b1ed4fbfc2b8665d6bcb2
Juliana Ribeiro <jmbrito.ribeirodev@gmail.com>
2026-04-29 00:40:42 -0300

    calcula o salario por hora trabalhada

---
+++ dia_1/ex4_salario_por_hora_trabalhada.py
@@ -0,0 +1,8 @@
+#Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+#Calcule e mostre o total do seu salário no referido mês.
+
+print('CALCULO DO SALÁRIO')
+valorhora = float(input('Quando você ganha por hora? '))
+horastrab = float(input('Quantas horas você trabalhou no mês? '))
+salario = valorhora * horastrab
+print('Seu salário este mês é R$',salario)
